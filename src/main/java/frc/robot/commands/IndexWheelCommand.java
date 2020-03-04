0adb041fc29a5baa38d360346b7cb22329aaea68
vygL <valleylin410@gmail.com>
2020-03-04 15:07:49 -0800

    minor changes to PID control and Index wheel changes

--- src/main/java/frc/robot/commands/IndexWheelCommand.java
+++ src/main/java/frc/robot/commands/IndexWheelCommand.java
@@ -28,7 +28,7 @@ public class IndexWheelCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    new WaitCommand(5);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
